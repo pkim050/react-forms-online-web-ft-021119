c246ffce66dc3b92e7a73206f45e780adebc623c
Patrick Kim <pkim050@gmail.com>
2019-07-18 12:05:39 +0000

    Automatically backed up by Learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -10,6 +10,11 @@ class ControlledInput extends React.Component {
     });
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    this.sendFormDataSomewhere(this.state)
+  }
+
   render() {
     return (
       <form onSubmit={event => this.handleSubmit(event)}>
